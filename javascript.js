ad57017b0a90af3b13eaa57bda3627639e2d1d73
Sean <palmsmp1@live.com>
2022-07-15 15:10:03 -0500

    Add computer choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-console.log("hello world!")
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let randomChoice = (choices[Math.floor(Math.random() * choices.length)]);
+    return randomChoice;
+}
+
