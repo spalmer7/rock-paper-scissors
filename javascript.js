2a0adea91a9f6b00af6c6bacffaaeb76a304e212
Sean <palmsmp1@live.com>
2022-07-16 22:43:27 -0500

    Standardize quotes

--- javascript.js
+++ javascript.js
@@ -3,14 +3,17 @@ function computerPlay() {
     let randomChoice = (choices[Math.floor(Math.random() * choices.length)]);
     return randomChoice;
 }
+
+computerPlay();
+
 function singleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("Draw!");
-    } else if ((playerSelection === "rock" && computerSelection == "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
-        console.log("You win!")
+        console.log('Draw!');
+    } else if ((playerSelection === 'rock' && computerSelection == 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === "paper" && computerSelection === "rock")) {
+        console.log('You win!')
     } else {
-        console.log("You lose!")
+        console.log('You lose!')
     }
 }
 
-singleRound("scissors", "scissors")
+singleRound('rock', computerPlay);
