d0075180d97dc4f5eeb5febd576a0b69f3d092ee
Sean Palmer <palmsmp1@live.com>
2022-07-19 16:28:15 -0500

    Add alert with game result

--- javascript.js
+++ javascript.js
@@ -7,20 +7,24 @@ function computerPlay() {
 function singleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log(`Draw! ${playerSelection} equals ${computerSelection}`)
+        alert(`Draw! ${playerSelection} equals ${computerSelection}`)
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === "paper" && computerSelection === "rock")) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
-    } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection=== 'paper') || (computerSelection === "paper" && playerSelection === "rock")) {
+        alert(`You win! ${playerSelection} beats ${computerSelection}!`)
+    } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === "paper" && playerSelection === "rock")) {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}!`)
+        alert(`You lose! ${computerSelection} beats ${playerSelection}!`)
     } else {
         console.log('Please sumbit a valid option!')
+        alert('Please sumbit a valid option!')
     }
 }
 
-function game(){
+function game() {
     for (let i = 1; i <= 5; i++) {
         let playerChoice = prompt('Rock, Paper, or Scissors?');
         let playerChoiceLower = playerChoice.toLowerCase();
-        singleRound(playerChoiceLower, computerPlay()); 
+        singleRound(playerChoiceLower, computerPlay());
     }
 }
 
