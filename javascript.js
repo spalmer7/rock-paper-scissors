5098752a44266605345212b5e7c8ce9d7e9d204d
Sean Palmer <palmsmp1@live.com>
2022-07-19 15:41:20 -0500

    Fix game logic and add game loop

--- javascript.js
+++ javascript.js
@@ -1,25 +1,24 @@
-let playerChoice = prompt('Rock, Paper, or Scissors?');
-let playerChoiceLower = playerChoice.toLowerCase();
-
 function computerPlay() {
     const choices = ['rock', 'paper', 'scissors'];
     let randomChoice = (choices[Math.floor(Math.random() * choices.length)]);
     return randomChoice;
 }
 
-computerPlay();
-
 function singleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log(`Draw! ${playerSelection} equals ${computerSelection}`)
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === "paper" && computerSelection === "rock")) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
-    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === "paper" && computerSelection === "rock")) {
+    } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection=== 'paper') || (computerSelection === "paper" && playerSelection === "rock")) {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}!`)
     } else {
         console.log('Please sumbit a valid option!')
     }
 }
 
-singleRound(playerChoiceLower, computerPlay()); 
+for (let i = 1; i <= 5; i++) {
+    let playerChoice = prompt('Rock, Paper, or Scissors?');
+    let playerChoiceLower = playerChoice.toLowerCase();
+    singleRound(playerChoiceLower, computerPlay()); 
+}
 
