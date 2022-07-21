8c1c44dcc50adfaf3483af7e6ffe73bc1e1d81ca
Sean Palmer <palmsmp1@live.com>
2022-07-21 16:23:35 -0500

    Move score function

--- javascript.js
+++ javascript.js
@@ -14,35 +14,27 @@ function singleRound(playerSelection, computerSelection) {
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === "paper" && computerSelection === "rock")) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
         alert(`You win! ${playerSelection} beats ${computerSelection}!`)
+        playerScore = playerScore + 1;
+        console.log(`Player Score: ${playerScore}!`)
     } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === "paper" && playerSelection === "rock")) {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}!`)
         alert(`You lose! ${computerSelection} beats ${playerSelection}!`)
+        computerScore = computerScore + 1;
+        console.log(`Computer Score: ${computerScore}!`)
     } else {
         console.log('Please sumbit a valid option!')
         alert('Please sumbit a valid option!')
     }
 }
 
-function score(playerSelection, computerSelection) {
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === "paper" && computerSelection === "rock")) {
-      playerScore = playerScore + 1;
-      console.log(`Player Score: ${playerScore}!`)
-    } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === "paper" && playerSelection === "rock")) {
-      computerScore = computerScore + 1;
-      console.log(`Computer Score: ${computerScore}!`)
-    } else {
-
-    }
-}
 function game() {
     for (let i = 1; i <= 5; i++) {
         let playerChoice = prompt('Rock, Paper or Scissors?');
-            if (playerChoice === null) {
-                break;
-            }
+        if (playerChoice === null) {
+            break;
+        }
         let playerChoiceLower = playerChoice.toLowerCase();
         singleRound(playerChoiceLower, computerPlay());
-        score(playerChoiceLower, computerPlay());
     }
 }
 
