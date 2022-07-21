52b22924c868324dee2678371bb155e42b42e02e
Sean Palmer <palmsmp1@live.com>
2022-07-21 16:52:36 -0500

    Add score alerts

--- javascript.js
+++ javascript.js
@@ -13,17 +13,17 @@ function computerPlay() {
 function singleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log(`Draw! ${playerSelection} equals ${computerSelection}`)
-        alert(`Draw! ${playerSelection} equals ${computerSelection}`)
+        alert(`Draw! ${playerSelection} equals ${computerSelection}\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`)
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === "paper" && computerSelection === "rock")) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
-        alert(`You win! ${playerSelection} beats ${computerSelection}!`)
         playerScore = playerScore + 1;
         console.log(`Player Score: ${playerScore}!`)
+        alert(`You win! ${playerSelection} beats ${computerSelection}!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`)
     } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === "paper" && playerSelection === "rock")) {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}!`)
-        alert(`You lose! ${computerSelection} beats ${playerSelection}!`)
         computerScore = computerScore + 1;
         console.log(`Computer Score: ${computerScore}!`)
+        alert(`You lose! ${computerSelection} beats ${playerSelection}!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`)
     } else {
         console.log('Please sumbit a valid option!')
         alert('Please sumbit a valid option!')
