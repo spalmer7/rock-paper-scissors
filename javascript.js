997c007bdaf1f3a282b2029b9483073d774dcf38
Sean Palmer <palmsmp1@live.com>
2022-07-22 16:57:43 -0500

    Eliminate extra alert

--- javascript.js
+++ javascript.js
@@ -12,25 +12,33 @@ function computerPlay() {
 // This function plays through a round and keeps score.
 function singleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log(`Draw! ${playerSelection} equals ${computerSelection}`)
-        alert(`Draw! ${playerSelection} equals ${computerSelection}\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`)
-    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
-            || (playerSelection === 'scissors' && computerSelection === 'paper') 
-            || (playerSelection === "paper" && computerSelection === "rock")) {
+        console.log(`Draw! ${playerSelection} equals ${computerSelection}`);
+        alert(`Draw! ${playerSelection} equals ${computerSelection}\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === "paper" && computerSelection === "rock")) {
         console.log(`You win! ${playerSelection} beats ${computerSelection}!`)
-        playerScore = playerScore + 1;
-        console.log(`Player Score: ${playerScore}!`)
-        alert(`You win! ${playerSelection} beats ${computerSelection}!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`)
-    } else if ((computerSelection === 'rock' && playerSelection === 'scissors') 
-            || (computerSelection === 'scissors' && playerSelection === 'paper') 
-            || (computerSelection === "paper" && playerSelection === "rock")) {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}!`)
-        computerScore = computerScore + 1;
-        console.log(`Computer Score: ${computerScore}!`)
-        alert(`You lose! ${computerSelection} beats ${playerSelection}!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`)
+        playerScore++;
+        console.log(`Player Score: ${playerScore}!`);
+        if (playerScore === 3) {
+            alert(`You win! Game Over!\nFinal Score\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+        } else {
+            alert(`You win! ${playerSelection} beats ${computerSelection}!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
+        }
+    } else if ((computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper') ||
+        (computerSelection === "paper" && playerSelection === "rock")) {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+        computerScore++;
+        console.log(`Computer Score: ${computerScore}!`);
+        if (computerScore === 3) {
+            alert(`You lose, Game Over!\nFinal Score\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+        } else {
+            alert(`You lose! ${computerSelection} beats ${playerSelection}!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
+        }
     } else {
-        console.log('Please sumbit a valid option!')
-        alert('Please sumbit a valid option!')
+        console.log('Please sumbit a valid option!');
+        alert('Please sumbit a valid option!');
     }
 }
 
@@ -41,11 +49,8 @@ function game() {
         if (playerChoice === null) {
             break;
         }
-        let playerChoiceLower = playerChoice.toLowerCase();
-        singleRound(playerChoiceLower, computerPlay());
-        if ((playerScore === 3) || (computerScore === 3)) {
-            alert(`Game Over! Final Score\nPlayer: ${playerScore}\nComputer: ${computerScore}`)
-        }
+        playerChoice = playerChoice.toLowerCase();
+        singleRound(playerChoice, computerPlay());
     }
 }
 
